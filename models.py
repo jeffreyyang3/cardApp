8c3d0d339ef3f63521f508b41c5e4b8bba1b1369
jeffreyyang3 <jeffreyhasmail@gmail.com>
2018-09-06 22:28:48 -0700

    half game, some style

--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
-
+import json
+import random
 
 author = 'Your name here'
 
@@ -18,12 +19,53 @@ class Constants(BaseConstants):
 
 
 class Subsession(BaseSubsession):
+    def creating_session(self):
+        self.group_randomly()
+        print('creating subsession called')
+        if('answers' not in self.session.vars or 
+        len(self.session.vars['answers']) < (10 * (len(self.get_players) - 1))):
+            answerFile = open("cardApp/cards/answers.txt", "r")
+            self.session.vars['answers'] = answerFile.readlines()
+            answerFile.close()
+            random.shuffle(self.session.vars['answers'])
+
+        
+
+
+        
+
+  
+
+        
+        numPlayers = len(self.get_players())
+        for i in range(numPlayers):
+            if(i == 0):
+                self.get_players()[i].isCz = True
+                
+            else:
+                answerDraw = []
+                for x in range(10):
+                    answerDraw.append(self.session.vars['answers'].pop())
+                self.get_players()[i].providedCards = json.dumps(answerDraw)
+                
+
+                self.get_players()[i].isCz = False
+        
+        
+
+            
+            
     pass
 
 
 class Group(BaseGroup):
+    question = models.TextField()
     pass
 
 
 class Player(BasePlayer):
+    isCz = models.BooleanField()
+    providedCards = models.TextField()
+    question = models.TextField()
+    chosenAnswer = models.TextField()
     pass
--- pages.py
+++ pages.py
@@ -1,24 +1,65 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
+import random
+import json
 
 
-class MyPage(Page):
-    pass
+class CzDraw(Page):
+    form_model = 'group'
+    form_fields = ['question']
 
+    def is_displayed(self):
+        return self.player.isCz
 
-class ResultsWaitPage(WaitPage):
+    def vars_for_template(self):
+        if 'questions' not in self.session.vars or len(self.session.vars['questions']) == 3:
+            questionFile = open("cardApp/cards/questions.txt", "r")
+            self.session.vars['questions'] = questionFile.readlines()
+            questionFile.close()
+            random.shuffle(self.session.vars['questions'])
+        questionList = []
+        questionList.append(self.session.vars['questions'].pop())
+        questionList.append(self.session.vars['questions'].pop())
+        questionList.append(self.session.vars['questions'].pop())
+            
+        return{
+            'questionList': questionList
+        }
+        
+
+class ChooseAnswer(Page):
+    form_model = 'player'
+    form_fields = ['chosenAnswer']
+    def is_displayed(self):
+        return not self.player.isCz
+    
+    def vars_for_template(self):
+        jsonDec = json.decoder.JSONDecoder()
+        newList = jsonDec.decode(self.player.providedCards)
+        return{
+            'question': self.group.question,
+            'answerDraw': newList
+        }
+
+class WaitForQuestion(WaitPage):
+    
 
     def after_all_players_arrive(self):
         pass
 
+class WaitForAnswers(WaitPage):
+    def after_all_players_arrive(self):
+        pass
 
 class Results(Page):
     pass
 
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
+    CzDraw,
+    WaitForQuestion,
+    ChooseAnswer,
+    WaitForAnswers,
     Results
 ]
